37005594ca37701b9b9ee6de28dd3b678b7ef94a
C0C41N <51693606+C0C41N@users.noreply.github.com>
2024-11-08 23:14:57 +0500

    chore: minor

--- app/src/main/java/com/example/poctangem/Action.kt
+++ app/src/main/java/com/example/poctangem/Action.kt
@@ -24,45 +24,42 @@ class Action {
 
     private val sdk = TangemSdkProvider.getInstance()
 
-    fun scan() {
+    fun scan() { GlobalScope.launch(Dispatchers.Main) {
 
-        GlobalScope.launch(Dispatchers.Main) {
+        val startSessionResult = sdk.startSessionAsync(null, null, accessCode = "141414")
 
-            val startSessionResult = sdk.startSessionAsync(null, null, accessCode = "141414")
-
-            if (!startSessionResult.success || startSessionResult.value == null) {
-                Log.e(logTag,"Start Session failed: ${startSessionResult.error}")
-                return@launch
-            }
-
-            val session = startSessionResult.value!!
-
-            val scanTask = ScanTask()
-            val scanResult = scanTask.runAsync(session)
-
-            if (!scanResult.success || scanResult.value == null) {
-                Log.e(logTag, "ScanTask failed: ${scanResult.error}")
-                session.stop()
-                return@launch
-            }
-
-            val card = scanResult.value!!
-
-            Log.d(logTag, card.toJson())
-
-            for (wallet in card.wallets) {
-                val curve = wallet.curve.name
-                val pubKey = wallet.publicKey.encodeToBase58String()
-                Log.d(logTag, "Wallet $curve | $pubKey")
-            }
-
-            session.stop()
+        if (!startSessionResult.success || startSessionResult.value == null) {
+            Log.e(logTag, "Start Session failed: ${startSessionResult.error}")
+            return@launch
         }
 
-    }
+        val session = startSessionResult.value!!
+
+        val scanTask = ScanTask()
+        val scanResult = scanTask.runAsync(session)
+
+        if (!scanResult.success || scanResult.value == null) {
+            Log.e(logTag, "ScanTask failed: ${scanResult.error}")
+            session.stop()
+            return@launch
+        }
+
+        val card = scanResult.value!!
+
+        Log.d(logTag, card.toJson())
+
+        for (wallet in card.wallets) {
+            val curve = wallet.curve.name
+            val pubKey = wallet.publicKey.encodeToBase58String()
+            Log.d(logTag, "Wallet $curve | $pubKey")
+        }
+
+        session.stop()
+
+    } }
 
     @OptIn(ExperimentalStdlibApi::class)
-    fun sign() {
+    fun sign() { GlobalScope.launch(Dispatchers.Main) {
 
         val publicKeyString = "AXfqRy8Jw3M4JhH1ea29YZH69hQqjndpgBpBSoRVLvYn"
         val unsignedHexString = "01000103c6dadd07fa6b967f95c1c794207a6660b6c103bb3d9225cb65a32aec9233bd4a7851663184f478288effadd0b24e403c625569350166ae9dfc10e1eaf4a203b000000000000000000000000000000000000000000000000000000000000000003aab9ecdda6344c5dea7dc04242579a2171d7e0b7659ac1d16b20ab1f00ba77901020200010c020000001027000000000000"
@@ -70,110 +67,101 @@ class Action {
         val publicKey = publicKeyString.decodeBase58()
         val unsignedHex = unsignedHexString.hexToByteArray()
 
-        GlobalScope.launch(Dispatchers.Main) {
+        val startSessionResult = sdk.startSessionAsync(null, null, accessCode = "141414")
 
-            val startSessionResult = sdk.startSessionAsync(null, null, accessCode = "141414")
+        if (!startSessionResult.success || startSessionResult.value == null) {
+            Log.e(logTag,"Start Session failed: ${startSessionResult.error}")
+            return@launch
+        }
 
-            if (!startSessionResult.success || startSessionResult.value == null) {
-                Log.e(logTag,"Start Session failed: ${startSessionResult.error}")
-                return@launch
-            }
+        val session = startSessionResult.value!!
 
-            val session = startSessionResult.value!!
+        val signTask = SignCommand(arrayOf(unsignedHex), publicKey)
+        val signResult = signTask.runAsync(session)
 
-            val signTask = SignCommand(arrayOf(unsignedHex), publicKey)
-            val signResult = signTask.runAsync(session)
+        if (!signResult.success || signResult.value == null) {
+            Log.e(logTag,"SignTask failed: ${signResult.error}")
+            session.stop()
+            return@launch
+        }
 
-            if (!signResult.success || signResult.value == null) {
-                Log.e(logTag,"SignTask failed: ${signResult.error}")
+        val signature = signResult.value!!.signatures[0]
+        Log.d(logTag, "Signed Hex | ${signature.toHexString()}")
+
+        session.stop()
+
+    } }
+
+    fun purgeAllWallets() { GlobalScope.launch(Dispatchers.Main) {
+
+        val startSessionResult = sdk.startSessionAsync(null, null, accessCode = "141414")
+
+        if (!startSessionResult.success || startSessionResult.value == null) {
+            Log.e(logTag,"Start Session failed: ${startSessionResult.error}")
+            return@launch
+        }
+
+        val session = startSessionResult.value!!
+
+        val scanTask = ScanTask()
+        val scanResult = scanTask.runAsync(session)
+
+        if (!scanResult.success || scanResult.value == null) {
+            Log.e(logTag,"ScanTask failed: ${scanResult.error}")
+            session.stop()
+            return@launch
+        }
+
+        val card = scanResult.value!!
+
+        for (wallet in card.wallets) {
+            val purgeTask = PurgeWalletCommand(wallet.publicKey)
+            val purgeResult = purgeTask.runAsync(session)
+
+            if (!purgeResult.success || purgeResult.value == null) {
+                Log.e(logTag,"PurgeTask failed: ${purgeResult.error}")
                 session.stop()
                 return@launch
             }
 
-            val signature = signResult.value!!.signatures[0]
-            Log.d(logTag, "Signed Hex | ${signature.toHexString()}")
+            val curve = wallet.curve.name
+            val pubKey = wallet.publicKey.encodeToBase58String()
 
-            session.stop()
+            Log.d(logTag, "Purged wallet $curve | $pubKey")
         }
 
-    }
+        session.stop()
 
-    fun purgeAllWallets() {
+    } }
 
-        GlobalScope.launch(Dispatchers.Main) {
+    fun createAllWallets() { GlobalScope.launch(Dispatchers.Main) {
 
-            val startSessionResult = sdk.startSessionAsync(null, null, accessCode = "141414")
+        val startSessionResult = sdk.startSessionAsync(null, null, accessCode = "141414")
 
-            if (!startSessionResult.success || startSessionResult.value == null) {
-                Log.e(logTag,"Start Session failed: ${startSessionResult.error}")
-                return@launch
-            }
+        if (!startSessionResult.success || startSessionResult.value == null) {
+            Log.e(logTag,"Start Session failed: ${startSessionResult.error}")
+            return@launch
+        }
 
-            val session = startSessionResult.value!!
+        val session = startSessionResult.value!!
+        val curves: List<EllipticCurve> = listOf(EllipticCurve.Secp256k1, EllipticCurve.Ed25519, EllipticCurve.Bls12381G2Aug, EllipticCurve.Bip0340, EllipticCurve.Ed25519Slip0010)
 
-            val scanTask = ScanTask()
-            val scanResult = scanTask.runAsync(session)
+        for(curve in curves) {
+            val createWallet = CreateWalletTask(curve)
+            val createWalletResult = createWallet.runAsync(session)
 
-            if (!scanResult.success || scanResult.value == null) {
-                Log.e(logTag,"ScanTask failed: ${scanResult.error}")
+            if (!createWalletResult.success || createWalletResult.value == null) {
+                Log.e(logTag,"CreateWalletTask failed: ${createWalletResult.error}")
                 session.stop()
                 return@launch
             }
 
-            val card = scanResult.value!!
+            val wallet = createWalletResult.value!!.wallet
 
-            for (wallet in card.wallets) {
-                val purgeTask = PurgeWalletCommand(wallet.publicKey)
-                val purgeResult = purgeTask.runAsync(session)
-
-                if (!purgeResult.success || purgeResult.value == null) {
-                    Log.e(logTag,"PurgeTask failed: ${purgeResult.error}")
-                    session.stop()
-                    return@launch
-                }
-
-                val curve = wallet.curve.name
-                val pubKey = wallet.publicKey.encodeToBase58String()
-
-                Log.d(logTag, "Purged wallet $curve | $pubKey")
-            }
-
-            session.stop()
+            Log.d(logTag, "Created wallet ${wallet.curve.name} | ${wallet.publicKey.encodeToBase58String()}")
         }
 
-    }
+        session.stop()
 
-    fun createAllWallets() {
-
-        GlobalScope.launch(Dispatchers.Main) {
-
-            val startSessionResult = sdk.startSessionAsync(null, null, accessCode = "141414")
-
-            if (!startSessionResult.success || startSessionResult.value == null) {
-                Log.e(logTag,"Start Session failed: ${startSessionResult.error}")
-                return@launch
-            }
-
-            val session = startSessionResult.value!!
-            val curves: List<EllipticCurve> = listOf(EllipticCurve.Secp256k1, EllipticCurve.Ed25519, EllipticCurve.Bls12381G2Aug, EllipticCurve.Bip0340, EllipticCurve.Ed25519Slip0010)
-
-            for(curve in curves) {
-                val createWallet = CreateWalletTask(curve)
-                val createWalletResult = createWallet.runAsync(session)
-
-                if (!createWalletResult.success || createWalletResult.value == null) {
-                    Log.e(logTag,"CreateWalletTask failed: ${createWalletResult.error}")
-                    session.stop()
-                    return@launch
-                }
-
-                val wallet = createWalletResult.value!!.wallet
-
-                Log.d(logTag, "Created wallet ${wallet.curve.name} | ${wallet.publicKey.encodeToBase58String()}")
-            }
-
-            session.stop()
-        }
-
-    }
+    } }
 }
